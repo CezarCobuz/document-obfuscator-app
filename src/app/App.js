ee9f9ce810189fc8365562bebcdf66dc229aaf26
CezarCobuz <36707637+CezarCobuz@users.noreply.github.com>
2021-10-31 15:46:35 +0300

    Add initial bottom tabs navigator

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,32 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {HomeScreen} from '../screens/Home.screen';
 import {DocumentsWalletScreen} from '../screens/DocumentsWallet.screen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* TODO: Recycle stack navigator latter on  */}
+      {/* <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen
           name="DocumentsWalletScreen"
           component={DocumentsWalletScreen}
           options={{title: 'My Documents'}}
         />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen
+          name="DocumentsWalletScreen"
+          component={DocumentsWalletScreen}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
