2162d7b8baedfbb3bbf4be413aff13cc665df421
CezarCobuz <36707637+CezarCobuz@users.noreply.github.com>
2021-10-31 16:22:10 +0300

    Add rn-image-picker, dummy upload screen

--- src/screens/Wallet.screen.js
+++ src/screens/Wallet.screen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-export const DocumentsWalletScreen = () => {
+export const WalletScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Wallet Screen</Text>
